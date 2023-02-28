8007fd096ebea546d1910a4c41f1626558d33be9
Elis <eliso1190@yandex.ru>
2023-02-28 14:15:57 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"git", "kitty", "elis", "11", ":)"};
+string[] array2 = new string[array1.Length];
+void FinalArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void GetArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FinalArray(array1, array2);
+GetArray(array2);
